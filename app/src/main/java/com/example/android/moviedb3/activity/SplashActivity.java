32bbc61d86f2a3f8208c6a51e7737528b8209d08
Nugroho Dewantoro <nugroho.dewantoro@outlook.com>
2017-08-31 14:40:49 +0700

    Add loading text in splash activity

--- app/src/main/java/com/example/android/moviedb3/activity/SplashActivity.java
+++ app/src/main/java/com/example/android/moviedb3/activity/SplashActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.moviedb3.activity;
 
+import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
+import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.android.moviedb3.R;
 import com.example.android.moviedb3.activityShifter.ActivityLauncher;
@@ -20,20 +25,53 @@ import com.example.android.moviedb3.movieDB.MovieData;
 import com.example.android.moviedb3.movieDB.MovieDataURL;
 
 import java.util.ArrayList;
+import java.util.logging.LogRecord;
 
 public class SplashActivity extends AppCompatActivity {
 
+    TextView stillLoadingTextView;
+    ConstraintLayout splashActivityLayout;
     int amountDataObtained;
+
+    private Runnable visibleLoadingTextRunnable = new Runnable() {
+        @Override
+        public void run()
+        {
+            stillLoadingTextView.setVisibility(View.VISIBLE);
+
+            Handler handler = new Handler();
+            handler.postDelayed(callMainMovieObtainedListener, 3000);
+        }
+    };
+
+    private Runnable callMainMovieObtainedListener = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            MainMovieListObtainedListener mainMovieListObtainedListener = new MainMovieListObtainedListener();
+            mainMovieListObtainedListener.onComplete(true);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        stillLoadingTextView = (TextView) findViewById(R.id.txt_still_loading_splash_activity);
+        splashActivityLayout = (ConstraintLayout) findViewById(R.id.splash_activity_layout);
+
+        TransitionManager.beginDelayedTransition(splashActivityLayout);
+        stillLoadingTextView.setVisibility(View.GONE);
+
         amountDataObtained = 0;
 
         GetNowShowingMovieList();
         GetComingSoonMovieList();
+
+        Handler handler = new Handler();
+        handler.postDelayed(visibleLoadingTextRunnable, 5000);
     }
 
     private void GetNowShowingMovieList()
@@ -83,7 +121,7 @@ public class SplashActivity extends AppCompatActivity {
         @Override
         public void onComplete(boolean isSuccess)
         {
-            if(amountDataObtained >= 1)
+            if(amountDataObtained >= 2)
             {
                 ActivityLauncher.LaunchActivity(new DefaultIActivityLauncher(MovieListActivity.class, SplashActivity.this));
             }
@@ -94,4 +132,8 @@ public class SplashActivity extends AppCompatActivity {
             }
         }
     }
+
+
+
+
 }
