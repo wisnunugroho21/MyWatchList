2e8334208c7756c8f41fa636f72ac858a196186e
Nugroho Dewantoro <nugroho.dewantoro@outlook.com>
2017-09-02 05:10:15 +0700

    fix the function of no reviews

---
+++ app/src/main/java/com/example/android/moviedb3/adapter/RecyclerViewAdapter/MainMovieListRecyclerViewAdapter.java
@@ -0,0 +1,50 @@
+package com.example.android.moviedb3.adapter.RecyclerViewAdapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.moviedb3.R;
+import com.example.android.moviedb3.eventHandler.OnDataChooseListener;
+import com.example.android.moviedb3.movieDB.MovieData;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nugroho on 04/07/17.
+ */
+
+public class MainMovieListRecyclerViewAdapter extends RecyclerView.Adapter<MainMovieListRecyclerViewHolder>
+{
+    private ArrayList<MovieData> movieDataArrayList;
+    private Context context;
+    private OnDataChooseListener<MovieData> movieDataOnDataChooseListener;
+
+    public MainMovieListRecyclerViewAdapter(ArrayList<MovieData> movieDataArrayList, Context context, OnDataChooseListener<MovieData> movieDataOnDataChooseListener)
+    {
+        this.movieDataArrayList = movieDataArrayList;
+        this.context = context;
+        this.movieDataOnDataChooseListener = movieDataOnDataChooseListener;
+    }
+
+    @Override
+    public MainMovieListRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_movie_item_list, parent, false);
+        return new MainMovieListRecyclerViewHolder(view, context, movieDataOnDataChooseListener);
+    }
+
+    @Override
+    public void onBindViewHolder(MainMovieListRecyclerViewHolder holder, int position)
+    {
+        holder.Bind(movieDataArrayList.get(position));
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return movieDataArrayList.size();
+    }
+}
